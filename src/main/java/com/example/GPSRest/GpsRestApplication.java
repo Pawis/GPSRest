70ea1c87252e26797036ea5ffb8376d7e2f81cbf
Pawis <Pawis88888@gmail.com>
2022-01-19 21:12:32 +0100

    initial commit

---
+++ src/main/java/com/example/GPSRest/GpsRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.GPSRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsRestApplication.class, args);
+	}
+
+}
